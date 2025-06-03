ec6c94a1a66f2c2794cf583d51981e8b94a185ac
valentinoaraya <valentinoaraya04@gmail.com>
2025-06-03 09:31:55 -0300

    Redirijo al nuevo portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Main from './components/Main/Main.jsx'
-import Project1Detail from './components/DetailedProjects/Project1Detail/Project1Detail.jsx'
-import Project2Detail from './components/DetailedProjects/Project2Detail/Project2Detail.jsx'
-import Project3Detail from './components/DetailedProjects/Project3Detail/Project3Detail.jsx'
+import Home from './components/Home/Home'
 
 function App() {
 
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/project-detail/sportymaxx' element={<Project1Detail />} />
-          <Route path='/project-detail/turnero-pami' element={<Project2Detail />} />
-          <Route path='/project-detail/ventatrack' element={<Project3Detail />} />
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </div>
   )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
 import "./Home.css"
 import Button from '../common/Button/Button.jsx';
-import "animate.css"
 
 const Home = () => {
     return (
         <div className='mainPageContainer'>
             <main className='mainPage'>
                 <div className='divInfoContainer'>
-                    <h1 className='principalTitle'>Hola, soy Valentino Araya</h1>
-                    <h2 className='secundaryTitle'>Full Stack Developer</h2>
-                    <p className='mainP'>Amo convertir ideas en experiencias digitales intuitivas y atractivas.</p>
+                    <h1 className='principalTitle'>Hola, parece que encontraste mi antiguo portfolio.</h1>
                     <div className='buttonContainer'>
-                        <Button download={true}>Descargar CV</Button>
+                        <Button
+                            onClick={() => window.open('https://valentinoaraya.vercel.app/', '_blank')}
+                        >
+                            Presiona aquí para ir al actual
+                        </Button>
                     </div>
                 </div>
             </main>
